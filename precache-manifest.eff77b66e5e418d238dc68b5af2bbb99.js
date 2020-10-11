d59e9c9ba72a9abc74116bce7ebd73fcbf534c95
Rahul Chavda <rchavda88@gmail.com>
2020-10-11 23:47:45 +0530

    build

---
+++ precache-manifest.eff77b66e5e418d238dc68b5af2bbb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aff7ed1e7b866a7f660dbd460d6ae69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c88c2adee596e2c9eb64",
+    "url": "/static/css/main.efa1332c.chunk.css"
+  },
+  {
+    "revision": "96817521315a228b626e",
+    "url": "/static/js/2.f8883ede.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8883ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88c2adee596e2c9eb64",
+    "url": "/static/js/main.d521759a.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);
